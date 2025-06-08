01f365f03f51b1ee024adc96236392c7875e225d
AthifahNurRahmanMD <athifaahnur145@gmail.com>
2025-06-08 13:42:54 +0800

    feat:(networking) add API configuration file (RetrofitClientInstance)

---
+++ app/src/main/java/com/example/tmflix/networking/RetrofitClientInstance.java
@@ -0,0 +1,18 @@
+package com.example.tmflix.networking;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClientInstance {
+    private static Retrofit retrofit;
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(ApiConfig.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
